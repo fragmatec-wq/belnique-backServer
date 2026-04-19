5d675d78debb3238a7bdcb22e5d0d6c607a10832
fragmatec-wq <fragmatec@gmail.com>
2026-04-19 22:23:07 +0100

    Delete routes directory

--- routes/conferenceRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMessages, sendMessage } = require('../controllers/conferenceController');
-
-router.get('/:lessonId/messages', getMessages);
-router.post('/:lessonId/messages', sendMessage);
-
-module.exports = router;
